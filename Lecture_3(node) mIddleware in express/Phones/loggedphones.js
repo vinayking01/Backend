6dbf7da4c8a7a0b247e5ba3dd5ca993d795ad649
Vinay Singh <vinaysinghcktd01@gmail.com>
2024-11-13 16:34:08 +0530

    code corrected

--- Lecture_3(node) mIddleware in express/Phones/loggedphones.js
+++ Lecture_3(node) mIddleware in express/Phones/loggedphones.js
@@ -1,21 +1,22 @@
 const express = require('express');
-const router = express.Router();  //This is a built-in middleware function in Express
+const router = express.Router();  // This creates a router instance for handling routes
 
-var phone_name  = "samsung";
+var phone_name = "samsung";  // Phone name defined globally in this file
 
+// Handle GET request to /phone-detail/Name
+router.get('/Name', (req, res, next) => {
+    console.log(`Bought SmartPhones: ${phone_name}`);
+    res.send(`Bought SmartPhones: ${phone_name}`);
+});
 
-router.get('/Name',(req,res,next)=>{
-    console.log(`Boughts SmartPhones ${phone_name}`);
-    res.send(`Boughts SmartPhones ${phone_name}`);
-})
+// Handle GET request to /phone-detail/
+router.get('/', (req, res) => {
+    res.send("What details do you want? \n 1. Name \n 2. Price");
+});
 
+// Handle GET request to /phone-detail/price
+router.get('/price', (req, res) => {
+    res.send("Price of SmartPhones: $77272");
+});
 
-router.get('/',(req,res)=>{
-    res.send("What details you want \n 1.Name \n 2.Price")
-})
-
-router.get('/price',(req,res)=>{
-    res.send("Price of SmartPhones $77272")
-})
-
-module.exports = router;
+module.exports = router;  // Export the router so it can be used in other files
--- Lecture_3(node) mIddleware in express/index.js
+++ Lecture_3(node) mIddleware in express/index.js
@@ -1,9 +1,5 @@
 //Topic  - Middleware
  //1. A middleware is a function that hooks into the routing process, performing an arbitrary operation at some point in the chain (depending on what we want it to do). 
- //2. Every middleware should ends with either next() function inside the body or by sending the response otherwise it won't move to the next step and give you the error .
- //3. Middleware are Prebuilt which you can install using the same way as packages ,or you can also create the custom middleware .
- //4. Middleware executes in synchronized manner first come first serve.
- //5. Midddleware should always end either with response or next() which leads to the next middleware/routes.
 
 var express = require('express')
 var app = express();
@@ -16,61 +12,36 @@ app.use(BodyParser.json())  // it intercepts the data sent by client, if it is i
 var f = "Vinay SIngh"
 
 
-app.get('/login', (req, res) => {
+app.get('/login', (req, res,next) => {
   console.log("welcome to login page");
   res.send("you have logged in ")
-  
 })
 
-
-
- // A) First example  - if you attached the multiple middleware , for passing to the next middleware you must use next() function and if you send the response to the server in any middleware it will stop their and skip the remaining middleware.
-
-/* app.use((req,res,next)=>{
-    // res.send("You are in middleware ")
-    console.log("In middleware 1")
-    next();  // the next function says move to next middleware otherwise the Routes. if you forgot to mention it along with any kind of response it will lead you to the error.
-
-})
-
-app.use((req,res,next)=>{
-    // res.send("You are in middleware 2")
-    console.log("In middleware 2")
-    next();  // the next function says move to next middleware otherwise the Routes. if you forgot to mention it along with any kind of response it will lead you to the error.
-
-})
-
- app.get('/', function (req, res) {   
-    res.send('hello world')
-  })
- */
  
-  // B) Second Example -- if you have send the response to the client first than you are attaching middleware after that, it won't work for the that type of route in between you want to attach the middleware.
-  
- /* app.get('/', function (req, res) {   
-    res.send('hello world')
-  })
-
-  app.use((req,res,next)=>{
-    // res.send("You are in middleware ")
-    console.log("In middleware 1")
-    next();  // the next function says move to next middleware otherwise the Routes. if you forgot to mention it along with any kind of response it will lead you to the error.
+app.use((req,res,next)=>{
+  // res.send("You are in middleware ")
+  console.log("In middleware 1")
+  next();  // the next function says move to next middleware otherwise the Routes. if you forgot to mention it along with any kind of response it will lead you to the error.
 
 })
 
 app.use((req,res,next)=>{
-    // res.send("You are in middleware 2")
-    console.log("In middleware 2")
-    next();  // the next function says move to next middleware otherwise the Routes. if you forgot to mention it along with any kind of response it will lead you to the error.
+  // res.send("You are in middleware 2")
+  console.log("In middleware 2")
+  next();  // the next function says move to next middleware otherwise the Routes. if you forgot to mention it along with any kind of response it will lead you to the error.
 
 })
 
-*/
+app.get('/', function (req, res) {   
+  res.send('hello world')
+
+})
+  
 
 
-  //(C) Third Example -  // Define middleware function
+//(C) Third Example -  // Define middleware function
 
-/*  const logTime = (req, res, next) => {
+ const logTime = (req, res, next) => {
     console.log('Time:', Date.now());
     next();
   };
@@ -78,22 +49,13 @@ app.use((req,res,next)=>{
     res.send('Hello, Person!');
   });
   
-*/
-
 
   //(D) Fourth Example -  if wanted to route a details of samsung but not all this in index.js files so you can route it in this way which contain all routes related to samsung only . for this require router middleware use. routing to the other files but for the same category 
 
-  /*
+  const Details = require('./Phones/loggedphones');
   
-  
-  */
-  var Details =require('./Phones/loggedphones');
-  
-  app.use('/phone-detail',Details);
+  app.use('/phone-detail', Details);
 
-  app.get('/',(req,res)=>{
-    res.send("everythng completed");
-  });
 
 
 app.listen(Port) 
